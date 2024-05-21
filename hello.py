d3a3542ca029d078d8119b25c3303733fd9ba508
Aboateng5 <168710350+Aboateng5@users.noreply.github.com>
2024-05-21 15:03:25 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,9 +3,11 @@ def hello_world():
     rs = print(results)
     return rs
 
+hello_world()
 
 def hello_world_n(N):
     results = "Hello World! "*N
     rs_n = print(results)
     return rs_n
 
+hello_world_n(N)
