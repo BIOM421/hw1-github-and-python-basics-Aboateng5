46cdf0d4cb7b78812399df494af992d57f0c7470
Aboateng5 <aboateng5@student.gsu.edu>
2024-05-21 16:29:16 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 def hello_world():
-	return "Hello World!"
-
+    results = "Hello World!"
+    rs = print(results)
+    return rs
 
 
 def hello_world_n(N):
-	return "Hello World! "*N
-
+    results = "Hello World! "*N
+    rs_n = print(results)
+    return rs_n
